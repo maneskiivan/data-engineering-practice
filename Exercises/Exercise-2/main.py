e87d15b88db7bda641cbc1e11da85bd17e3457e8
maneskiivan <maneskiivan@gmail.com>
2022-06-01 09:45:16 -0700

    Added new functions

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,5 +1,7 @@
+import os
+
 import requests
-import pandas
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
@@ -26,15 +28,40 @@ def find_file_name(soup: BeautifulSoup, dt: str) -> str:
   return 'not found'
 
 
+def create_dir(path: str) -> str:
+  # Create downloads directory if there isn't one already
+  if not os.path.isdir(path):
+    os.mkdir(path)
+
+  return path
+
+
+def clear_old_data(path: str) -> None:
+  if os.path.exists(path):
+    os.remove(path)
+
+
+def download_files(uri: str, file_name: str, directory: str) -> str:
+  response = send_request(uri)
+  file_name = directory + file_name
+  clear_old_data(file_name)
+  with open(file_name, 'wb') as f:
+    f.write(response.content)
+    f.close()
+  if os.path.exists(file_name):
+    return file_name
+
+
 def main():
     # your code here
     uri = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
     dt = '2022-02-07 14:03'
     data = send_request(uri)
     soup = create_soup(data.text)
-    download_link = uri + find_file_name(soup, dt)
-
-
+    file_name = find_file_name(soup, dt)
+    download_link = uri + file_name
+    download_dir = create_dir('./downloads/')
+    download_file = download_files(download_link, file_name, download_dir)
 
 
 if __name__ == '__main__':
