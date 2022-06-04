6bfdd45b1885ae13fc843adae0395f0bdaa2de2a
maneskiivan <maneskiivan@gmail.com>
2022-06-03 17:48:35 -0700

    Added code to download the .gz file and read it in memory as bytes

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,9 +1,17 @@
 import boto3
 
+import io
+import gzip
+
 
 def main():
-    # your code here
-    pass
+  bucket = 'commoncrawl'
+  key = 'crawl-data/CC-MAIN-2022-05/wet.paths.gz'
+  file = key.rsplit('/', 1)[1]
+
+  s3 = boto3.client('s3')
+  response_obj = s3.get_object(Bucket=bucket, Key=key)
+  object_content = response_obj['Body'].read()
 
 
 if __name__ == '__main__':
