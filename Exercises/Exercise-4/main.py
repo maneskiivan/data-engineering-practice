d4213a55f0008eb371f79802c8d02ac3846a5d56
maneskiivan <maneskiivan@gmail.com>
2022-06-05 09:44:28 -0700

    Added code to convert from json to csv

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -2,20 +2,45 @@ from flatten_json import flatten
 
 import os
 import json
+import csv
 
 
-def main():
+def find_json(parent_dir: str) -> dict:
   json_files = dict()
-  for root, subdirs, files in os.walk('data/'):
+  for root, subdirs, files in os.walk(parent_dir):
     for file in files:
       if '.json' in file:
         json_files[file] = root.rstrip('/')
 
+  return json_files
+
+
+def json_to_csv(json_file: dict, csv_file: str) -> str:
+  data_file = open(csv_file, 'w', newline='')
+  csv_writer = csv.writer(data_file)
+
+  header = json_file.keys()
+  csv_writer.writerow(header)
+  csv_writer.writerow(json_file.values())
+
+  data_file.close()
+
+  if os.path.exists(csv_file):
+    return csv_file
+
+
+def main():
+  json_files = find_json('data/')
+
   for fn, directory in json_files.items():
-    full_path = f'{directory}/{fn}'
-    with open(full_path, 'r') as f:
+    json_path = f'{directory}/{fn}'
+    csv_path = json_path.rstrip('.json')
+    csv_path = csv_path + '.csv'
+
+    with open(json_path, 'r') as f:
       flat_json = flatten(json.load(f))
-      print(flat_json)
+      json_to_csv(flat_json, csv_path)
+
 
 if __name__ == '__main__':
     main()
