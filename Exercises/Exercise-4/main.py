29eeeb7188d6576b2ef2ff799a024b044711aa95
maneskiivan <maneskiivan@gmail.com>
2022-06-04 22:12:48 -0700

    Identified the json files and flattened them

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,10 +1,21 @@
-import boto3
+from flatten_json import flatten
+
+import os
+import json
 
 
 def main():
-    # your code here
-    pass
+  json_files = dict()
+  for root, subdirs, files in os.walk('data/'):
+    for file in files:
+      if '.json' in file:
+        json_files[file] = root.rstrip('/')
 
+  for fn, directory in json_files.items():
+    full_path = f'{directory}/{fn}'
+    with open(full_path, 'r') as f:
+      flat_json = flatten(json.load(f))
+      print(flat_json)
 
 if __name__ == '__main__':
     main()
