64054aed4d64be85ae5ea27a59c3ac92604a7ad9
maneskiivan <maneskiivan@gmail.com>
2022-06-05 21:03:25 -0700

    Added the part that reads the sql stmts from the .sql files

--- Exercises/Exercise-5/main.py
+++ Exercises/Exercise-5/main.py
@@ -1,14 +1,35 @@
 import psycopg2
 
+import os
+
+
+def read_sql_file(path: str) -> str:
+  sql_stmt = ''
+  with open(path, 'r') as f:
+    for line in f.readlines():
+      sql_stmt += line
+
+  return sql_stmt
+
+
+
 
 def main():
-    host = 'postgres'
+    host = 'localhost'
     database = 'postgres'
     user = 'postgres'
     pas = 'postgres'
     conn = psycopg2.connect(host=host, database=database, user=user, password=pas)
-    # your code here
 
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    sql_stmt_dir = './data/sql/'
+    for root, subdirs, sql_stmt_files in os.walk(sql_stmt_dir):
+      for file in sql_stmt_files:
+        cursor.execute(read_sql_file(sql_stmt_dir + file))
+
+    conn.close()
 
 if __name__ == '__main__':
     main()
