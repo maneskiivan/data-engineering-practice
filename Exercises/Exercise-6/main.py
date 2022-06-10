9e58fcdd522b74e4900e2cf66d6cc102b8a95650
maneskiivan <maneskiivan@gmail.com>
2022-06-09 21:36:16 -0700

    Added initial code that reads the zip file and extracts the csv content into a pandas dataframe then converts it to spark dataframe

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -1,10 +1,22 @@
 from pyspark.sql import SparkSession
+import pandas as pd
+
+from zipfile import ZipFile
+import io
 
 
 def main():
-    spark = SparkSession.builder.appName('Exercise6') \
-        .enableHiveSupport().getOrCreate()
-    # your code here
+  spark = SparkSession.builder.appName('Exercise6') \
+    .enableHiveSupport().getOrCreate()
+
+  pd_df = None
+  with ZipFile('data/Divvy_Trips_2019_Q4.zip') as z:
+    with z.open('Divvy_Trips_2019_Q4.csv') as f:
+      pd_df = pd.read_csv(f)
+
+  sp_df = spark.createDataFrame(pd_df.astype(str))
+  print(sp_df.show())
+
 
 
 if __name__ == '__main__':
