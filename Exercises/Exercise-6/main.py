f002feb828c14335930b22c716f5f8a6b4febb1d
Ivan Maneski <maneskiivan@gmail.com>
2022-06-14 21:50:31 -0700

    Answered question 4

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.window import Window
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType, FloatType, TimestampType
 import pandas as pd
 
@@ -47,19 +48,29 @@ def main():
   q1 = sp_df.groupBy(date_format(col('start_time'), 'yyyy-MM-dd').alias('Date')) \
       .agg(format_number(avg('tripduration'), 2).alias('Trip Duration per day')).orderBy('Date')
 
-  print(q1.show())
+  q1.show()
 
   # 2. How many trips were taken each day?
   q2 = sp_df.groupBy(date_format(col('start_time'), 'yyyy-MM-dd').alias('Date')) \
       .agg(count('trip_id').alias('Trips per day')).orderBy('Date')
 
-  print(q2.show())
+  q2.show()
 
   # 3. What was the most popular starting trip station for each month?
   q3 = sp_df.groupBy(date_format(col('start_time'), 'MM').alias('Month')) \
-      .agg(max('from_station_name').alias('Most popular station name')).orderBy('Month')
+      .agg(max('from_station_name').alias('Most popular starting station name')).orderBy('Month')
 
-  print(q3.show())
+  q3.show()
+
+  # 4. What were the top 3 trip stations each day for the last two weeks?
+  window_spec = Window.partitionBy(date_format(col('start_time'), 'yyyy-MM-dd')).orderBy(col('from_station_name'))
+  q4 = sp_df.withColumn("rank", dense_rank().over(window_spec))
+
+  q4.select(
+    date_format(col('start_time'), 'yyyy-MM-dd').alias('Date'),
+    col('from_station_name'),
+    col('rank')
+  ).distinct().filter(col("rank") <= 3).orderBy('Date', 'rank').show()
 
 
 if __name__ == '__main__':
